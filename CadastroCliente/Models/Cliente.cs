a2b196e10cff7dcdd8d6f2f11f79b5312a02e966
mattheusbr <matheusferreirabruno@hotmail.com>
2021-01-07 19:17:18 -0300

    init

---
+++ CadastroCliente/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CadastroCliente.Models
+{
+    [Table("Cliente")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Sexo Sexo { get; set; }
+
+    }
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminimo = 2
+    }
+}
